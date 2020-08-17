d35ed2a33f59c366324e6e2b6d45abe7cf42ee95
Akhil-EM <akhilemcalicut@gmail.com>
2020-08-17 13:32:17 -0700

    hosting attempt-5

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express=require('express');
-const path=require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-const app=express();
+const app = express();
 
-app.use(express.static(__dirname+'/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/gym-anytime'));
 
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/gym-anytime/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
